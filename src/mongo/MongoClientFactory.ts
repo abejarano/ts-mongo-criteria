264ba11e618597e9d899c85d53e4cf43e53e5e3b
abejarano <programador.angel@gmail.com>
2026-02-01 07:39:52 -0300

    fix: Variable pattern to connect with mongo server.

--- src/mongo/MongoClientFactory.ts
+++ src/mongo/MongoClientFactory.ts
@@ -30,18 +30,18 @@ export class MongoClientFactory {
    * Crea y conecta una nueva instancia de MongoClient.
    */
   private static async createAndConnectClient(): Promise<MongoClient> {
-    const MONGO_PASS = process.env.MONGO_PASS
-    const MONGO_USER = process.env.MONGO_USER
-    const MONGO_DB = process.env.MONGO_DB
-    const MONGO_SERVER = process.env.MONGO_SERVER
 
-    if (!MONGO_PASS || !MONGO_USER || !MONGO_DB || !MONGO_SERVER) {
-      throw new Error("Missing MongoDB environment variables.")
+    const uri = process.env.MONGO_URI
+
+    if (!uri) {
+      throw new Error(
+        "MONGO_URI environment variables are missing to connect to the MongoDB server"
+      )
     }
 
-    const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_SERVER}/${MONGO_DB}?retryWrites=true&w=majority`
-
-    const client = new MongoClient(uri, { ignoreUndefined: true })
+    const client = new MongoClient(process.env.MONGO_URI!, {
+      ignoreUndefined: true,
+    })
 
     try {
       await client.connect()
